b319a3cdcf063cb26da595ca2155202694e84069
Eduardo Alves Serqueira <eduardo.serqueira@outlook.com>
2022-11-21 16:10:21 -0300

    Add campo cpf

--- DataBase/EFCursoContext.cs
+++ DataBase/EFCursoContext.cs
@@ -1,9 +1,12 @@
-﻿using Microsoft.EntityFrameworkCore;
+﻿using EFCurso.Models;
+using Microsoft.EntityFrameworkCore;
 
 namespace EFCurso.DataBase
 {
     public class EFCursoContext : DbContext
     {
         public EFCursoContext(DbContextOptions<EFCursoContext> options) : base(options) {}
+
+        public DbSet<Funcionario> Funcionarios { get; set; }
     }
 }
